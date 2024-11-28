109540970f1bf15ad25923d56def08b57923cd97
barrosofernandez <roberto.barroso@geociencias.unam.mx>
2024-11-28 03:34:32 -0600

    thing

--- barroso-stats-proyecto-final.R
+++ barroso-stats-proyecto-final.R
@@ -4,7 +4,7 @@
 
 # Made by: Roberto Barroso Fernández
 # Instituto de Geociencias UNAM
-# 
+#
 # Proyecto final: Herramientas Estadísticas de las Geociencias
 
 # ######################################### #
@@ -12,7 +12,8 @@
 # ######################################### #
 
 # Reemplazar path por el preferido
-setwd('/Users/roberto/Documents/stats-final-project')
+# setwd("/Users/roberto/Documents/stats-final-project")
+setwd("/Users/robby/Documents/stats-final-project")
 
 library(geodata)
 library(ggplot2)
@@ -20,40 +21,40 @@ library(sf)
 library(dplyr)
 library(scales)
 
-######### ######### ######### ######### ######### ######### ######### ######### 
+######### ######### ######### ######### ######### ######### ######### #########
 
 # ######################################### #
 #            Defining functions             #
 # ######################################### #
 
-plims <- function(x,p=0.1) {
-	# limits of plot axis
-	# x - numeric
-	# p - percentage (from 0 to 1) of padding
-	maxval <- max(x)
-	minval <- min(x)
-	span   <- maxval-minval
-	lim_hi <- maxval + 0.5 * p * span
-	lim_lo <- minval - 0.5 * p * span
-	return (c(lim_lo,lim_hi))
+plims <- function(x, p = 0.1) {
+    # limits of plot axis
+    # x - numeric
+    # p - percentage (from 0 to 1) of padding
+    maxval <- max(x)
+    minval <- min(x)
+    span   <- maxval - minval
+    lim_hi <- maxval + 0.5 * p * span
+    lim_lo <- minval - 0.5 * p * span
+    return(c(lim_lo, lim_hi))
 }
 
-magfreqs <- function(m,mbin=0.1) {
+magfreqs <- function(m, mbin = 0.1) {
 	# Generates data.frame of number of events against magnitude
 	# Input:
 	# m - list of magnitudes
 	# mbin - binning width (difference)
 	# Output:
-	# df$magnitude - 
-	# df$amount - 
-	mags = seq(round(min(m),digits=1), round(max(m),digits=1), mbin)
+	# df$magnitude -
+	# df$amount -
+	mags = seq(round(min(m), digits = 1), round(max(m), digits = 1), mbin)
 	amt = 1:length(mags)
 	for (i in 1:length(mags)){
 		cutmag = mags[i]
 		amt[i] = length(m[m>cutmag])
 	}
-	df <- data.frame(magnitude=mags,amount=amt)
-	return (df)
+	df <- data.frame(magnitude = mags, amount = amt)
+	return(df)
 }
 
 kagan <- function(x) {
@@ -96,64 +97,64 @@ timediff <- function(date1,date2) {
 	} else if (delta_unit == 'hours') {
 		delta_val <- delta_val / 24
 	} 
-	return (delta_val)
+	return(delta_val)
 }
 
 
-######### ######### ######### ######### ######### ######### ######### ######### 
+######### ######### ######### ######### ######### ######### ######### #########
 
 # Definin Gardner-Knopoff (1974) windows
 
 # If declustering this way, it is assumed that events that aren't
 # clustered are completely independent, which is probably not the case
-# for Mexico (or at least, the Mexican Subduction Zone, MSZ). 
-# Declustering based on Kagan radii (Avila-Barrientos et al., 2015), 
+# for Mexico (or at least, the Mexican Subduction Zone, MSZ).
+# Declustering based on Kagan radii (Avila-Barrientos et al., 2015),
 # then some other models, would be better, but due to time constraints,
 # I am using Gardner-Knopoff. Hope this helps :)
 
 # magnitudes
-gk_m <- c(2.5,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5,7.0,7.5,8.0) 
+gk_m <- c(2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0)
 # radii in kilometers
-gk_l <- c(19.5,22.5,26,30,35,40,47,54,61,70,81,94)
+gk_l <- c(19.5, 22.5, 26, 30, 35, 40, 47, 54, 61, 70, 81, 94)
 # time intervals in days
-gk_t <- c(6,11.5,22,42,83,155,290,510,790,915,960,985)
+gk_t <- c(6, 11.5, 22, 42, 83, 155, 290, 510, 790, 915, 960, 985)
 
-gk <- data.frame(m=gk_m,l=gk_l,t=gk_t)
+gk <- data.frame(m = gk_m, l = gk_l, t = gk_t)
 
-######### ######### ######### ######### ######### ######### ######### ######### 
+######### ######### ######### ######### ######### ######### ######### #########
 
 # ######################################### #
 #           Processing the catalog          #
 # ######################################### #
 
 # Loading the catalog
-sismos <- read.csv("catalogo_sismos.csv",skip=4)[-(304906:304912),] %>%
-          filter(Magnitud != 'no calculable')
-sismos$utc <- paste(sismos$Fecha.UTC,sismos$Hora.UTC) %>% 
-              strptime('%Y-%m-%d %H:%M:%S') %>%
-              as.POSIXct(tz = 'UTC')
+sismos <- read.csv("catalogo_sismos.csv", skip = 4)[-(304906:304912), ] %>%
+    filter(Magnitud != "no calculable")
+sismos$utc <- paste(sismos$Fecha.UTC,sismos$Hora.UTC) %>%
+    strptime("%Y-%m-%d %H:%M:%S") %>%
+    as.POSIXct(tz = "UTC")
 sismos$Magnitud <- as.numeric(sismos$Magnitud)
 sismo_coords <- sph_to_rct(
-	sismos$Latitud, sismos$Longitud, sismos$Profundidad
+    sismos$Latitud, sismos$Longitud, sismos$Profundidad
 )
 sismos$X <- sismo_coords$X
 sismos$Y <- sismo_coords$Y
-sismos$Z <- sismo_coords$Z 
+sismos$Z <- sismo_coords$Z
 
 # relative time to first event in catalog, in days
 time <- 1:length(sismos$X)
 for (i in 1:length(sismos$X)) {
-	date1 = sismos$utc[1]
-	date2 = sismos$utc[i]
-	time[i] <- timediff(date1,date2) 
+    date1 = sismos$utc[1]
+    date2 = sismos$utc[i]
+    time[i] <- timediff(date1, date2)
 }
 sismos$time <- time
 
 # First step: taking into account completeness of catalog
-mc = 3.1
-mc_bool = sismos$Magnitud >= mc
+mc <- 3.1
+mc_bool <- sismos$Magnitud >= mc
 sismos_mc <- sismos[mc_bool,]   # complete for older data
-sismos_mc_len = length(sismos_mc[,1])
+sismos_mc_len <- length(sismos_mc[, 1])
 sismos_mc$idx <- 1:sismos_mc_len
 
 # Second step: removing aftershocks
@@ -165,54 +166,55 @@ sismos_mc$idx <- 1:sismos_mc_len
 
 # Using the Gardner-Knopoff algorithm
 
-gk_rad <- spline(x=gk$m, y=gk$l, method='natural', xout=sismos_mc$Magnitud)$y
-gk_tdy <- spline(x=gk$m, y=gk$t, method='natural', xout=sismos_mc$Magnitud)$y
+gk_rad <- spline(x = gk$m, y = gk$l, method = "natural",
+                 xout = sismos_mc$Magnitud)$y
+gk_tdy <- spline(x = gk$m, y = gk$t, method = "natural",
+                 xout = sismos_mc$Magnitud)$y
 
 # 0 if not an aftershock, 1 if yes
 aftershocks <- rep(0,sismos_mc_len)
 for (ii in 1:sismos_mc_len) {
-	if (ii %% 5000 == 0){
-		print(ii)
-	}
-	if (ii == sismos_mc_len){
-		sprintf('DONE!')
-		break
-	}
-	mag <- sismos_mc$Magnitud[ii]
-	rad <- gk_rad[ii]  # radius
-	tdy <- gk_tdy[ii]  # time
-	# if the event isn't already marked as an aftershock
-	if (aftershocks[ii] == 0) {
-		jj <- ii + 1
-		# it has to fall within the acceptable time window for
-		# declustering
-		while ((sismos_mc$time[jj] - sismos_mc$time[ii]) < tdy) {
-			# could be more elegant
-			x1 = sismos_mc$X[ii] 
-			y1 = sismos_mc$Y[ii] 
-			z1 = sismos_mc$Z[ii]
-			x2 = sismos_mc$X[jj] 
-			y2 = sismos_mc$Y[jj] 
-			z2 = sismos_mc$Z[jj]
-			# distance between events
-			rad_diff = sqrt((x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2)
-			# it has to fall withing the acceptable radius for
-			# it to be marked as an aftershock  
-			if (rad_diff < rad) {
-				aftershocks[jj] = 1
-			}
-			if (jj == sismos_mc_len){
-				break
-			}
-			jj <- jj + 1
-		}
-	}
+    if (ii %% 5000 == 0){
+        print(ii)
+    }
+    if (ii == sismos_mc_len){
+        break
+    }
+    mag <- sismos_mc$Magnitud[ii]
+    rad <- gk_rad[ii]  # radius
+    tdy <- gk_tdy[ii]  # time
+    # if the event isn't already marked as an aftershock
+    if (aftershocks[ii] == 0) {
+        jj <- ii + 1
+        # it has to fall within the acceptable time window for
+        # declustering
+        while ((sismos_mc$time[jj] - sismos_mc$time[ii]) < tdy) {
+            # could be more elegant
+            x1 <- sismos_mc$X[ii]
+            y1 <- sismos_mc$Y[ii]
+            z1 <- sismos_mc$Z[ii]
+            x2 <- sismos_mc$X[jj]
+            y2 <- sismos_mc$Y[jj]
+            z2 <- sismos_mc$Z[jj]
+            # distance between events
+            rad_diff <- sqrt((x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2)
+            # it has to fall withing the acceptable radius for
+            # it to be marked as an aftershock
+            if (rad_diff < rad) {
+                aftershocks[jj] = 1
+            }
+            if (jj == sismos_mc_len){
+                break
+            }
+            jj <- jj + 1
+        }
+    }
 }
 
 sismos_mc$aftershocks <- aftershocks
 
-sismos_clust <- sismos_mc[sismos_mc$aftershocks==1,]
-sismos_bckgd <- sismos_mc[sismos_mc$aftershocks==0,]
+sismos_clust <- sismos_mc[sismos_mc$aftershocks == 1, ]
+sismos_bckgd <- sismos_mc[sismos_mc$aftershocks == 0, ]
 
 ######### ######### ######### ######### ######### ######### ######### #########
 
@@ -226,35 +228,119 @@ sismos_bckgd <- sismos_mc[sismos_mc$aftershocks==0,]
 # the other for deeper events
 # threshold set at 40 km, after Zuñiga et al. (2017)
 
-# Choosing X, Y, Z, magnitude
+# To perform:
+# Elbow test (author, year)
+# Gap test (Tibshirani et al. 2001, estiamting the number of clusters...)
+# Silhouette test (Rousseeuw, 1987)
+
+
+# Choosing X, Y, Z
 
 # Magnitude is a function of fault size anyway...
 
-cutoff_depth = 40
+cutoff_depth <- 40
 
-bool_depth = sismos_bckgd$Profundidad<=cutoff_depth
+bool_depth <- sismos_bckgd$Profundidad <= cutoff_depth
 
-shallow <- sismos_bckgd[bool_depth,
-                        c('Longitud','Latitud','Magnitud','X','Y','Z')]
-deep    <- sismos_bckgd[!bool_depth,
-                        c('Longitud','Latitud','Magnitud','X','Y','Z')]
+shallow <- sismos_bckgd[bool_depth, c("Longitud", "Latitud", "Magnitud",
+                                      "X", "Y", "Z")]
+deep <- sismos_bckgd[!bool_depth, c("Longitud", "Latitud", "Magnitud",
+                                    "X", "Y", "Z")]
+
+# For tests
+n_clust <- 2:20
+
+# For elbow test
+elbow_test <- seq(1, length(n_clust), 1)
+
+# For gap test
+gap_test <- seq(1, length(n_clust), 1)
+
+# For silhouette test
+silhouette_test <- seq(1, length(n_clust), 1)
+
+
+W_k_clust <- seq(1, length(n_clust), 1)
+W_k_random <- seq(1, length(n_clust), 1)
+B <- 10
+
+for (ii in seq.int(1, length(n_clust), 1)) {
+	k <- n_clust[ii]
+	clust <- kmeans(shallow[, c("X", "Y", "Z")], k, nstart = 25)
+	
+    # Elbow test
+	elbow_test[ii] <- clust$tot.withinss
+	
+    # Gap test
+    # Gap test: for our data cluster
+    clust_dist_sum <- seq(1, k, 1)
+    pooled <- seq(1, k, 1)
+    for (jj in seq(1, k, 1)) {
+        C_jj <- shallow[clust$cluster == jj, c("X", "Y", "Z")]
+        clust_dist <- dist(C_jj)
+        clust_dist_sum[jj] <- sum(clust_dist)
+        # Element for pooled within-cluster SS around cluster mean
+        pooled[jj] <- clust_dist_sum[jj] / (2 * clust$size[jj])
+    }
+    pooled_sum <- sum(pooled)  # W_k of our data cluster
+    # Gap test: for the random cluster
+    # Has to be repeated B times
+    pooled_random <- seq(1, B, 1)
+    for (kk in seq(1, B, 1)) {
+        x_random <- runif(n = length(shallow$X),
+                          min = min(shallow$X),
+                          max = max(shallow$X))
+        y_random <- runif(n = length(shallow$Y),
+                          min = min(shallow$Y),
+                          max = max(shallow$Y))
+        z_random <- runif(n = length(shallow$Z),
+                          min = min(shallow$Z),
+                          max = max(shallow$Z))
+        random <- data.frame(X = x_random, Y = y_random, Z = z_random)
+        clust_random <- kmeans(random, clust$centers)
+        clust_random_dist_sum <- seq(1, k, 1)
+        pooled_random_bit <- seq(1, k, 1)
+        for (ll in seq(1, k, 1)) {
+            C_ll <- random[clust_random$cluster == ll, ]
+            clust_random_dist <- dist(C_ll)
+            clust_random_dist_sum[ll] <- sum(clust_random_dist)
+            # Element for pooled within-cluster SS around cluster mean
+            pooled_random_bit[ll] <- clust_random_dist_sum[ll] /
+                                     (2 * clust_random$size[ll])
+        pooled_random_bit_sum <- sum(pooled_random_bit)  # W_kb of random clus
+        pooled_random[kk] <- pooled_random_bit_sum
+        }
+    }
+    gap_k <- (1 / B) * sum(log10(pooled_random)) - log10(pooled_sum)
+    gap_test[ii] <- gap_k
+
+
+}
+
+
+for (i in seq.int(1, length(n_clust), 1)) {
+	n <- n_clust[i]
+	clust <- kmeans(shallow[, c("X", "Y", "Z")], n, nstart = 25)
+}
+
+
 
 shallow_kmeans <- kmeans(
-	shallow[,c('Magnitud','X','Y','Z')],10,nstart=20
+	shallow[, c("X", "Y", "Z")], 10, nstart = 20
 )
-plot(shallow[,c('Longitud','Latitud')],col=shallow_kmeans$cluster)
+plot(shallow[, c("Longitud", "Latitud")], col = shallow_kmeans$cluster)
 
 ######### ######### ######### ######### ######### ######### ######### #########
 
-bckgnd_countries = c('US','GT','BZ','SV','HN','NI','CR','PA')
-bckgnd <- do.call('rbind',lapply(bckgnd_countries,
-                  function(x) gadm(country=x,level=0,resolution=2,
-                  path=getwd()))) %>%
-          st_as_sf(crs = st_crs('EPSG:6365'))
-mexico <- gadm(country='MX',level=1,resolution=2,path=getwd()) %>%
-          st_as_sf(crs = st_crs('EPSG:6365'))
-ev_pts <- st_as_sf(sismos_bckgd, coords=c('Longitud','Latitud'), 
-                   crs=st_crs('EPSG:6365'))
+bckgnd_countries <- c("US", "GT", "BZ", "SV", "HN", "NI", "CR", "PA")
+bckgnd <- do.call("rbind", lapply(bckgnd_countries,
+                  function(x) {gadm(country = x, level = 0,
+				  resolution = 2, path = getwd())})) %>%
+    st_as_sf(crs = st_crs("EPSG:6365"))
+mexico <- gadm(country = "MX", level = 1, resolution = 2, path = getwd()) %>%
+    st_as_sf(crs = st_crs("EPSG:6365"))
+ev_pts <- st_as_sf(sismos_bckgd, coords = c("Longitud", "Latitud"),
+                   crs = st_crs("EPSG:6365"))
 
 ######### ######### ######### ######### ######### ######### ######### #########
 
@@ -268,6 +354,6 @@ magmap1 <- ggplot() +
            geom_sf(data=ev_pts,alpha=0.2,
                    aes(color=Profundidad)) +
            coord_sf(xlim=plims(sismos$Longitud,p=-0.1),
-                    ylim=plims(sismos$Latitud,p=-0.1)) 
+                    ylim=plims(sismos$Latitud,p=-0.1))
 magmap1
 # No ejecutar esto a menos que esté bien arreglado
